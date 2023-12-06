1906f4a3238e752e0fa792ae406133281f7bc105
Filipe <filipemard@gmail.com>
2023-12-05 23:57:44 -0300

    update comments

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,30 @@
 "use strict";
+var __create = Object.create;
+var __defProp = Object.defineProperty;
+var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+var __getOwnPropNames = Object.getOwnPropertyNames;
+var __getProtoOf = Object.getPrototypeOf;
+var __hasOwnProp = Object.prototype.hasOwnProperty;
+var __copyProps = (to, from, except, desc) => {
+  if (from && typeof from === "object" || typeof from === "function") {
+    for (let key of __getOwnPropNames(from))
+      if (!__hasOwnProp.call(to, key) && key !== except)
+        __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
+  }
+  return to;
+};
+var __toESM = (mod, isNodeMode, target) => (target = mod != null ? __create(__getProtoOf(mod)) : {}, __copyProps(
+  // If the importer is in node compatibility mode or this is not an ESM
+  // file that has been converted to a CommonJS file using a Babel-
+  // compatible transform (i.e. "__esModule" has not been set), then set
+  // "default" to the CommonJS "module.exports" for node compatibility.
+  isNodeMode || !mod || !mod.__esModule ? __defProp(target, "default", { value: mod, enumerable: true }) : target,
+  mod
+));
+
+// index.ts
+var import_node_fs = __toESM(require("fs"));
+var import_node_path = __toESM(require("path"));
 
 // src/Utils.ts
 var Utils = {
@@ -110,12 +136,12 @@ function receiveToken(token, lines, column2) {
   console.log(lineTable);
   tokens.push(lineTable);
 }
-function Leitor(textContent2) {
-  if (typeof textContent2 !== "string")
+function Leitor(textContent) {
+  if (typeof textContent !== "string")
     return [];
   let word = "";
   let isComment = false;
-  const allContent = textContent2.split("");
+  const allContent = textContent.split("");
   allContent.forEach((letter, i) => {
     const nextLetter = allContent[++i];
     const nextTwoLetter = allContent[i + 1];
@@ -190,25 +216,14 @@ function Leitor(textContent2) {
 }
 
 // index.ts
-var textContent = `/** Faca uma funcao que calcule a area de um retangulo **/
-
-      nuque calculaArea1 (nuque base, nuque altura) {
-        nuque area = base * altura;
-        
-        expelliarmus area;
-      }
-      
-      int main(){
-        nuque base, altura, area1;
-      
-        lumus("Informe o valor da base(em metros): ");
-        truth base;
-      
-        area1 = calculaArea1(base, altura);
-      
-        lumus("A area do projeto e " + area1 + " metros");
-      
-        expelliarmus 0;
-      }
-      `;
-Leitor(textContent);
+var filePath = import_node_path.default.join(__dirname, "../exemplo.hpl");
+function main() {
+  import_node_fs.default.readFile(filePath, { encoding: "utf-8" }, function(err, textContent) {
+    if (!err) {
+      Leitor(textContent);
+    } else {
+      console.log(`N\xE3o foi possivel abrir o arquivo: ${err}`);
+    }
+  });
+}
+main();
